e3b599202be8902aa401fd7c199c09ff76e41d18
eugene663 <yujin663@gmail.com>
2022-07-07 18:43:25 +0900

    pagination fail

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ const badgeEl = document.querySelector('header .badges');
 window.addEventListener('scroll', _.throttle(function () {
   console.log(window.scrollY);
   if(window.scrollY > 500){ //배지 숨기기
-      //gsap.to(요소, 지속시간, 옵션);
+      //gsap.to(요소, 지속시간(초 단위), 옵션);
       gsap.to(badgeEl, .6, {
         opacity: 0,
         display: 'none'
@@ -34,3 +34,38 @@ window.addEventListener('scroll', _.throttle(function () {
   }
 }, 300));
 // _.throttle(함수, ms단위 시간)
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7,
+    opacity: 1
+  });
+});
+
+
+//new Swiper(선택자, 옵션);
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical', // 수직 슬라이드
+  autoplay: true, // 자동 재생 
+  loop: true // 반복 
+});
+
+new Swiper('.promotion .swiper-container',{
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수 
+  spaceBetween: 10, // 슬라이드 사이 여백 
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  loop: true,
+  autoplay: {
+    delay: 5000
+  },
+  paginatiion: {
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable: true // 사용자의 페이지 번호 요소 제어
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+});
