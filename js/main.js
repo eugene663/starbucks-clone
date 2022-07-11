45552c058d9a258be4ea4c4a2c5e9fc71ad3dabc
eugene663 <yujin663@gmail.com>
2022-07-11 18:33:50 +0900

    ScrollMagic

--- js/main.js
+++ js/main.js
@@ -83,3 +83,39 @@ promotionToggleBtn.addEventListener('click', function () {
     promotionEl.classList.remove('hide');
   }
 });
+
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+
+function floatingObject(selector, delay, size){
+  // gsap.to(요소, 시간, 옵션);
+  gsap.to(selector, // 선택자
+     random(1.5, 2.5), // 애니메이션 동작 시간
+     { // 옵션
+      y: 20, // y축
+      repeat: -1, // 무한 반복
+      yoyo: true, // 한 번 재생된 애니메이션을 뒤로 재생
+      ease: Power1.easeInOut, // 반복 자연스럽게
+      delay: random(0, delay)
+  });
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
+
+
+const spyEls = document.querySelectorAll('.section scroll-spy');
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic
+    .Scene({
+      triggerElement: spyEl, // 보여짐 여부를 감시할 요소 지정
+      triggerHook: .8 // 화면의 80% 지점에서 보여짐 여부 감시
+    })
+    .setClassToggle()
+    .addTo();
+});
